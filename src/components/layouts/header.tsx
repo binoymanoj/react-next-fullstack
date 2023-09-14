017fe47db245ef09061290e89d48658cc28d2a7e
binoymanoj <binoypm2002@gmail.com>
2023-09-14 21:53:58 +0530

    font change

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='h-14 bg-zinc-800 text-white px-4 flex items-center justify-between'>
-      <h2 className='text-2xl font-semibold '>
+      <h2 className='text-2xl font-semibold font-mono'>
         Binoy Manoj
       </h2>
       <h2 className='text-2xl font-semibold '>
